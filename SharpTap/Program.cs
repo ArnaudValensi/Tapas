bb82231916d0534587b258eb27f4a610c5d95bc4
Arnaud Valensi <arnaud.valensi@gmail.com>
2019-07-03 14:17:21 +0200

    Init

---
+++ SharpTap/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SharpTap
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //BenchmarkRunner.Run<RegisterDiagnostic>();
+        }
+    }
+}
